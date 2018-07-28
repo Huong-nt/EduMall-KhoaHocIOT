dfc48845b3dbf8ca6aa1011d7a06952aa92f3fc7
huong-nt <nguyentienhuong93@gmail.com>
2018-07-28 11:38:44 +0700

    update mqtt code

--- 8. Giới thiệu giao thức MQTT/code/Device/MQTT_Sub.ino
+++ 8. Giới thiệu giao thức MQTT/code/Device/MQTT_Sub.ino
@@ -10,17 +10,18 @@
 #include <PubSubClient.h>
 
 // Thông tin Wifi
-const char* ssid = "ssid"; // your wireless network name (SSID)
-const char* password = "password"; // your Wi-Fi network password
+const char *ssid = "ssid";         // your wireless network name (SSID)
+const char *password = "password"; // your Wi-Fi network password
 
-const int RELAY = 2; // Relay 1stconst char* mqtt_server = "test.mosquitto.org"; // Thông tin MQTT Broker
+const int RELAY = 2;                      // Relay 1stconst
+char *mqtt_server = "test.mosquitto.org"; // Thông tin MQTT Broker
 
 // Thông tin tin clientID, publishing, subcribing topic. ClientID nên là duy nhất trong tất cả các ứng dụng
 // Thay đổi thông tin ở đây tương ứng với thông tin các bạn muốn
-const char* clientID = "698ed4c6-dcbf-4bc8-9d53-da66a4f1ab83"; // Random UUID
+const char *clientID = "698ed4c6-dcbf-4bc8-9d53-da66a4f1ab83"; // Random UUID
 
 // const char* outTopic = "mqtt/esp/";
-const char* inTopic = "mqtt/esp/test";
+const char *inTopic = "mqtt/esp/test";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -28,7 +29,8 @@ char msg[50];
 int count = 0;
 
 // Hàm kết nối Wifi
-void setup_wifi() {
+void setup_wifi()
+{
 
   delay(10);
   // We start by connecting to a WiFi network
@@ -38,7 +40,8 @@ void setup_wifi() {
 
   WiFi.begin(ssid, password);
 
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
@@ -52,24 +55,29 @@ void setup_wifi() {
 
 // Hàm call back được gọi khi nhận được thông tin từ subcribing topic
 // Trong ứng dụng demo này thì chưa xử lý gì thông tin nhận được
-void callback(char* topic, byte* payload, unsigned int length) {
+void callback(char *topic, byte *payload, unsigned int length)
+{
   // xử lý dữ liệu nhận được
-  char* message = (char*) payload;
+  char *message = (char *)payload;
   Serial.println("Message: %s", message);
 }
 
 // Reconnect đến MQTT Broker
-void reconnect() {
+void reconnect()
+{
   // Loop until we're reconnected
-  while (!client.connected()) {
+  while (!client.connected())
+  {
     Serial.print("Attempting MQTT connection...");
     // Attempt to connect
-    if (client.connect(clientID)) {
+    if (client.connect(clientID))
+    {
       Serial.println("mqtt connected");
       // ... and resubscribe
       client.subscribe(inTopic);
     }
-    else {
+    else
+    {
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
@@ -79,17 +87,19 @@ void reconnect() {
   }
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   setup_wifi();
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
 }
 
-void loop() {
-  if (!client.connected()) {
+void loop()
+{
+  if (!client.connected())
+  {
     reconnect();
   }
   client.loop();
 }
-
